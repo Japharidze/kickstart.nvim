9b6cf2ad5b4e37a0a5e01cad9a5cc58523f1ffda
Sergi Japharidze <sergi.japharidze@gmail.com>
2024-03-10 05:12:45 +0400

    nvtree plugin set up

---
+++ lua/custom/settings.lua
@@ -0,0 +1,46 @@
+-- NvimTree Setup
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+
+local function my_on_attach(bufnr)
+  local api = require 'nvim-tree.api'
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts 'Up')
+  vim.keymap.set('n', '<Cr>', api.tree.change_root_to_node, opts 'Down')
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts 'Help')
+  vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+end
+
+require('nvim-tree').setup {
+  sort = {
+    sorter = 'case_sensitive',
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+  on_attach = my_on_attach,
+}
+
+-- Auto open NvimTree when Neovim starts
+vim.api.nvim_create_autocmd('VimEnter', {
+  pattern = '*',
+  command = 'NvimTreeOpen',
+})
